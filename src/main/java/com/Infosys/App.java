15012529fe377f3e3bf648cedaf2afa07b9e14aa
vberale <vberale@gmail.com>
2023-08-07 09:37:23 -0700

    file added successfully

---
+++ src/main/java/com/Infosys/App.java
@@ -0,0 +1,13 @@
+package com.Infosys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
